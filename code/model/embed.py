7cc23fa701ca3c62968faeaf1e698259ea1fe807
ryan-rozario <ryan.rozario1999@gmail.com>
2021-03-28 02:27:39 +0400

    Added embedding layer

--- code/model/embed.py
+++ code/model/embed.py
@@ -11,3 +11,33 @@ class Embedder(nn.Module):
         self.embed = nn.Embedding(vocab_size, d_model)
     def forward(self, x):
         return self.embed(x)
+
+#positional encoding gives input about the position of a word
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len = 80):
+        super().__init__()
+        self.d_model = d_model
+        
+        # create constant 'pe' matrix with values dependant on 
+        # pos and i
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = \
+                math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = \
+                math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+                
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+ 
+    
+    def forward(self, x):
+        # make embeddings relatively larger
+        x = x * math.sqrt(self.d_model)
+        #add constant to embedding
+        seq_len = x.size(1)
+        x = x + Variable(self.pe[:,:seq_len], \
+        requires_grad=False).cuda()
+        return x
